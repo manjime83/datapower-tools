1462b0ca92c2874fb576465b58a87b0e384a428d
Manuel Jiménez <man.jime@gmail.com>
2017-07-11 16:45:21 -0500

    template colp

---
+++ templater/assets/colpatria/template/db/PARAMPRO/mbc.${service_name}.sql
@@ -0,0 +1,3 @@
+INSERT INTO PARAMPRO.PAR_SERVICIO
+  VALUES ('${service_code}', '${service_desc}', 42, 0, 0, 1, 'Activo', 'Emir Cortes', 'Luis Fernando Martinez', NULL, NULL, 'jimenezma', CURRENT_DATE, NULL, NULL, 1, 1);
+COMMIT;
