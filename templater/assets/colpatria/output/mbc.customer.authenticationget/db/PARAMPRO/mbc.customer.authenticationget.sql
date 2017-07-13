017dbfd4070e953fcb9b4d8283131fa56a7522e0
Manuel Jiménez <man.jime@gmail.com>
2017-07-13 15:02:58 -0500

    template update

--- templater/assets/colpatria/output/mbc.customer.authenticationget/db/PARAMPRO/mbc.customer.authenticationget.sql
+++ templater/assets/colpatria/output/mbc.customer.authenticationget/db/PARAMPRO/mbc.customer.authenticationget.sql
@@ -1,3 +1,3 @@
 INSERT INTO PARAMPRO.PAR_SERVICIO
-  VALUES ('CANLGIN01', 'El usuario es un cliente del banco', 42, 0, 0, 1, 'Activo', 'Emir Cortes', 'Luis Fernando Martinez', NULL, NULL, 'jimenezma', CURRENT_DATE, NULL, NULL, 1, 1);
+  VALUES ('CANLGIN01', 'El usuario es un cliente del banco', 43, 0, 0, 1, 'Activo', 'Emir Cortes', 'Luis Fernando Martinez', NULL, NULL, 'jimenezma', CURRENT_DATE, NULL, NULL, 1, 1);
 COMMIT;
