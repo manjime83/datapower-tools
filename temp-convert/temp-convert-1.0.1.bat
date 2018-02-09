d7c3461b2cd26aceede24dc73b0ec64563833749
Manuel Jiménez <man.jime@gmail.com>
2018-02-09 13:04:50 -0500

    temp convert batch execution

--- temp-convert/temp-convert-1.0.1.bat
+++ temp-convert/temp-convert-1.0.1.bat
@@ -1,2 +1,3 @@
-"C:\Program Files\Java\jdk1.8.0_151\bin\java" -jar temp-convert-1.0.1.jar 8080
-pause
+@ECHO OFF
+SET "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_151"
+"%JAVA_HOME%\bin\java" -jar target\temp-convert-1.0.1.jar 8080
